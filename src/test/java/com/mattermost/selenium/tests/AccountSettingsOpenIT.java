813cb2d6507555f22779d29fbd2e260e17c39f09
lindalu <lindalu@users.noreply.github.com>
2017-02-15 11:16:57 -0800

    Create AccountSettingsOpenIT.java
    Opens and closes Account Settings.

---
+++ src/test/java/com/mattermost/selenium/tests/AccountSettingsOpenIT.java
@@ -0,0 +1,57 @@
+package com.mattermost.selenium.tests;
+
+import com.mattermost.selenium.DriverBase;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
+
+import org.testng.annotations.Test;
+
+import java.util.regex.Pattern;
+
+public class AccountSettingsOpenIT extends DriverBase {
+
+    @Test
+    public void testAccountSettingsOpenIT() throws Exception {        driver.get(baseUrl + "/login");
+        for (int second = 0;; second++) {
+        	if (second >= 60) fail("timeout");
+        	try { if (isElementPresent(By.cssSelector("button.btn.btn-primary"))) break; } catch (Exception e) {}
+        	Thread.sleep(1000);
+        }
+
+        driver.findElement(By.name("loginId")).clear();
+        driver.findElement(By.name("loginId")).sendKeys("test@test.com");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("passwd");
+        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+        for (int second = 0;; second++) {
+        	if (second >= 60) fail("timeout");
+        	try { if (isElementPresent(By.cssSelector("span.sidebar-header-dropdown__icon"))) break; } catch (Exception e) {}
+        	Thread.sleep(1000);
+        }
+
+        driver.findElement(By.cssSelector("span.sidebar-header-dropdown__icon")).click();
+        // General Settings
+        driver.findElement(By.xpath("//div[@id='sidebar-left']/div/div[2]/ul/li/a/span")).click();
+        for (int second = 0;; second++) {
+        	if (second >= 60) fail("timeout");
+        	try { if (isElementPresent(By.xpath("(//button[@type='button'])[11]"))) break; } catch (Exception e) {}
+        	Thread.sleep(1000);
+        }
+
+        driver.findElement(By.xpath("(//button[@type='button'])[11]")).click();
+        // Log out
+        driver.findElement(By.id("sidebar-header-dropdown")).click();
+        driver.findElement(By.xpath("//div[@id='sidebar-left']/div/div[2]/ul/li[4]/a")).click();
+        for (int second = 0;; second++) {
+        	if (second >= 60) fail("timeout");
+        	try { if (isElementPresent(By.name("loginId"))) break; } catch (Exception e) {}
+        	Thread.sleep(1000);
+        }
+
+   }
+}
