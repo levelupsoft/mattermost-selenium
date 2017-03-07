24a63a39e07b9732610d0fedaeccefd6d3c58c2a
lindalumitchell <linda@mattermost.com>
2017-03-06 22:47:20 -0800

    Update AccountSettingsFullNameIT.java
    Fewer waitForElements and remove failing logout.

--- src/test/java/com/mattermost/selenium/tests/AccountSettingsFullNameIT.java
+++ src/test/java/com/mattermost/selenium/tests/AccountSettingsFullNameIT.java
@@ -17,7 +17,7 @@ public class AccountSettingsFullNameIT extends DriverBase {
 
     @Test
     public void testAccountSettingsFullNameIT() throws Exception {        driver.get(baseUrl + "/login");
-        // DisableAnimations
+        disableAnimations();
         for (int second = 0;; second++) {
         	if (second >= 60) fail("timeout");
         	try { if (isElementPresent(By.cssSelector("button.btn.btn-primary"))) break; } catch (Exception e) {}
@@ -48,54 +48,30 @@ public class AccountSettingsFullNameIT extends DriverBase {
         driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[2]/a/span")).click();
         for (int second = 0;; second++) {
         	if (second >= 60) fail("timeout");
-        	try { if (isElementPresent(By.cssSelector("div.col-sm-7 > input.form-control"))) break; } catch (Exception e) {}
-        	Thread.sleep(1000);
-        }
-
-        driver.findElement(By.xpath("//input[@value='Save']")).click();
-        for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
-        	try { if ("Click 'Edit' to add your full name".equals(driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[3]/span")).getText())) break; } catch (Exception e) {}
-        	Thread.sleep(1000);
-        }
-
-        driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[2]/a/span")).click();
-        for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
-        	try { if (isElementPresent(By.cssSelector("div.col-sm-7 > input.form-control"))) break; } catch (Exception e) {}
+        	try { if ("First Name".equals(driver.findElement(By.cssSelector("label.col-sm-5.control-label")).getText())) break; } catch (Exception e) {}
         	Thread.sleep(1000);
         }
 
         driver.findElement(By.cssSelector("a.btn.btn-sm > span")).click();
-        for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
-        	try { if ("Click 'Edit' to add your full name".equals(driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[3]/span")).getText())) break; } catch (Exception e) {}
-        	Thread.sleep(1000);
-        }
-
         driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[2]/a/span")).click();
         for (int second = 0;; second++) {
         	if (second >= 60) fail("timeout");
-        	try { if (isElementPresent(By.cssSelector("div.col-sm-7 > input.form-control"))) break; } catch (Exception e) {}
+        	try { if ("First Name".equals(driver.findElement(By.cssSelector("label.col-sm-5.control-label")).getText())) break; } catch (Exception e) {}
         	Thread.sleep(1000);
         }
 
+        driver.findElement(By.cssSelector("div.col-sm-7 > input.form-control")).clear();
+        driver.findElement(By.cssSelector("div.col-sm-7 > input.form-control")).sendKeys("");
         driver.findElement(By.cssSelector("div.col-sm-7 > input.form-control")).sendKeys("Added");
-        driver.findElement(By.xpath("//input[@value='Save']")).click();
         for (int second = 0;; second++) {
         	if (second >= 60) fail("timeout");
-        	try { if ("Added".equals(driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[3]")).getText())) break; } catch (Exception e) {}
+        	try { if ("Last Name".equals(driver.findElement(By.xpath("//div[2]/label")).getText())) break; } catch (Exception e) {}
         	Thread.sleep(1000);
         }
 
-        driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[2]/a/span")).click();
-        for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
-        	try { if (isElementPresent(By.cssSelector("div.col-sm-7 > input.form-control"))) break; } catch (Exception e) {}
-        	Thread.sleep(1000);
-        }
-
-        driver.findElement(By.xpath("(//input[@value=''])[2]")).sendKeys("Name");
+        driver.findElement(By.xpath("//input[@value='LastName']")).clear();
+        driver.findElement(By.xpath("//input[@value='LastName']")).sendKeys("");
+        driver.findElement(By.xpath("//input[@value='LastName']")).sendKeys("Name");
         driver.findElement(By.xpath("//input[@value='Save']")).click();
         for (int second = 0;; second++) {
         	if (second >= 60) fail("timeout");
@@ -114,29 +90,6 @@ public class AccountSettingsFullNameIT extends DriverBase {
         for (int second = 0;; second++) {
         	if (second >= 60) fail("timeout");
         	try { if ("Edited LastName".equals(driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[3]")).getText())) break; } catch (Exception e) {}
-        	Thread.sleep(1000);
-        }
-
-        driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[2]/a/span")).click();
-        driver.findElement(By.cssSelector("div.col-sm-7 > input.form-control")).clear();
-        driver.findElement(By.cssSelector("div.col-sm-7 > input.form-control")).sendKeys("");
-        driver.findElement(By.cssSelector("div.col-sm-7 > input.form-control")).sendKeys(Keys.BACK_SPACE);
-        driver.findElement(By.xpath("//input[@value='LastName']")).clear();
-        driver.findElement(By.xpath("//input[@value='LastName']")).sendKeys("");
-        driver.findElement(By.xpath("//input[@value='LastName']")).sendKeys(Keys.BACK_SPACE);
-        driver.findElement(By.xpath("//input[@value='Save']")).click();
-        for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
-        	try { if ("Click 'Edit' to add your full name".equals(driver.findElement(By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div/div[2]/ul/li[3]/span")).getText())) break; } catch (Exception e) {}
-        	Thread.sleep(1000);
-        }
-
-        driver.findElement(By.xpath("(//button[@type='button'])[11]")).click();
-        driver.findElement(By.id("sidebar-header-dropdown")).click();
-        driver.findElement(By.xpath("//div[@id='sidebar-left']/div/div[2]/ul/li[4]/a/span")).click();
-        for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
-        	try { if (isElementPresent(By.name("loginId"))) break; } catch (Exception e) {}
         	Thread.sleep(1000);
         }
 
