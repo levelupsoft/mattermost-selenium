ea4df2af278c75fddb5a963c2e88273cea2c4534
lindalumitchell <linda@mattermost.com>
2017-04-04 21:34:34 -0700

    Update login steps

--- src/test/java/com/mattermost/selenium/tests/AccountSettingsOpenIT.java
+++ src/test/java/com/mattermost/selenium/tests/AccountSettingsOpenIT.java
@@ -16,11 +16,11 @@ import java.util.regex.Pattern;
 public class AccountSettingsOpenIT extends DriverBase {
 
     @Test
-    public void testAccountSettingsOpenIT() throws Exception {
+    public void testAccountSettingsOpenIT() throws Exception {        // LOG IN
         driver.get(baseUrl + "/login");
-        // DisableAnimations
+        disableAnimations();
         for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
+        	if (second >= 2) fail("timeout");
         	try { if (isElementPresent(By.cssSelector("button.btn.btn-primary"))) break; } catch (Exception e) {}
         	Thread.sleep(1000);
         }
@@ -29,33 +29,27 @@ public class AccountSettingsOpenIT extends DriverBase {
         driver.findElement(By.name("loginId")).sendKeys("test@test.com");
         driver.findElement(By.name("password")).clear();
         driver.findElement(By.name("password")).sendKeys("passwd");
-        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+        driver.findElement(By.id("loginButton")).click();
+        Thread.sleep(1000);
+        Thread.sleep(1000);
+        Thread.sleep(1000);
+        // LOG OUT
+        driver.navigate().refresh();
         for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
-        	try { if (isElementPresent(By.cssSelector("span.sidebar-header-dropdown__icon"))) break; } catch (Exception e) {}
+        	if (second >= 2) fail("timeout");
+        	try { if (isElementPresent(By.linkText("Town Square"))) break; } catch (Exception e) {}
         	Thread.sleep(1000);
         }
 
-        driver.findElement(By.cssSelector("span.sidebar-header-dropdown__icon")).click();
-        // General Settings
-        driver.findElement(By.xpath("//div[@id='sidebar-left']/div/div[2]/ul/li/a/span")).click();
+        driver.findElement(By.cssSelector("a.sidebar-header-dropdown__toggle")).click();
+        driver.findElement(By.cssSelector("#logout > span")).click();
+        Thread.sleep(1000);
         for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
-        	try { if ("Account Settings".equals(driver.findElement(By.xpath("//body/div[2]/div/div[2]/div/div/div/h4/span")).getText())) break; } catch (Exception e) {}
-        	Thread.sleep(1000);
-        }
-
-        driver.findElement(By.xpath("(//a[contains(text(),'General')])[2]")).click();
-        driver.findElement(By.xpath("(//button[@type='button'])[11]")).click();
-        // Sleep
-        // Log out
-        driver.findElement(By.id("sidebar-header-dropdown")).click();
-        driver.findElement(By.xpath("//div[@id='sidebar-left']/div/div[2]/ul/li[4]/a")).click();
-        for (int second = 0;; second++) {
-        	if (second >= 60) fail("timeout");
+        	if (second >= 2) fail("timeout");
         	try { if (isElementPresent(By.name("loginId"))) break; } catch (Exception e) {}
         	Thread.sleep(1000);
         }
 
+        driver.get(baseUrl + "/login");
     }
 }
