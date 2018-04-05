c40d5a05fcb8953fbadbebad2ab808593cd05614
lindalumitchell <linda@mattermost.com>
2018-04-05 10:30:10 -0700

    Locking to 4.9.0-rc2

--- setup-for-run.sh
+++ setup-for-run.sh
@@ -8,12 +8,12 @@ cp -f ~/mattermost/logs/mattermost.log $WORKSPACE/mm-latest.log
 rm -f mattermost.tar.gz
 
 # Regular daily
-wget https://releases.mattermost.com/mattermost-platform/master/mattermost-enterprise-linux-amd64.tar.gz
-mv mattermost-enterprise-linux-amd64.tar.gz mattermost.tar.gz
+# wget https://releases.mattermost.com/mattermost-platform/master/mattermost-enterprise-linux-amd64.tar.gz
+# mv mattermost-enterprise-linux-amd64.tar.gz mattermost.tar.gz
 
 # Use this to lock to specific version
-# wget https://releases.mattermost.com/4.8.0-rc6/mattermost-4.8.0-rc6-linux-amd64.tar.gz
-# mv mattermost-4.8.0-rc6-linux-amd64.tar.gz mattermost.tar.gz
+wget https://releases.mattermost.com/4.9.0-rc2/mattermost-4.9.0-rc2-linux-amd64.tar.gz
+mv mattermost-4.9.0-rc2-linux-amd64.tar.gz mattermost.tar.gz
 
 
 rm -rf ~/mattermost
